af0e7def2c3207f488d46de7141ad7d261cc6b3d
Cris Ward <cris@duodesign.co.uk>
2025-05-25 11:26:22 +0100

    added quick run test

--- src/App2.svelte
+++ src/App2.svelte
@@ -2,10 +2,15 @@
   import {count} from "./store.js";
   import Display from "./Display.svelte";
   let name ="World";
+
+  let rune = $state(5)
+
+
 </script>
 
 <hr>
 
-<button on:click={()=> $count+=1}>Add</button>
+<button onclick={()=> $count+=1}>Add</button>
+<button onclick={()=> rune+=1}>Rune Add ({rune})</button>
 
 <Display />
